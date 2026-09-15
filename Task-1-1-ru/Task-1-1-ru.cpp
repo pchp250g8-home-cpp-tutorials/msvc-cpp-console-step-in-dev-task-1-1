d83504e771e4fe007a90f12c57038f90fb73d984
Alexander <pchp250g8@tutamail.com>
2026-09-15 14:55:39 +0300

    Added new way to set russian locale

--- Task-1-1-ru/Task-1-1-ru.cpp
+++ Task-1-1-ru/Task-1-1-ru.cpp
@@ -11,8 +11,9 @@ int main(int argc, char* args[])
     unsigned int t; // Максимальное количество башен
     unsigned int b; // Количесво использованных блоков
     // Установка русской локали для терминала Windows 11
-    setlocale(LC_ALL, "russian.65001"); // 1 - й способ
-    std::locale rus_locale = std::locale("russian.65001"); // 2 - й способ
+    system("chcp 65001 >nul");// 1-й способ. Выплнить внешнюю команду.
+    setlocale(LC_ALL, "russian.65001"); // 2 - й способ. Вызов библиотечной функции
+    std::locale rus_locale = std::locale("russian.65001"); // 3 - й способ
     std::cout.imbue(std::locale::global(rus_locale)); // Выбор кодировки терминала
     /*Ввод значений переменных с клавиатуры*/
     std::cout << "Введите количество блоков: ";
